6077cf48b71f16076c6538f0db9aaf3448c2ea38
AnastasiyaPC <anastasiya.vynogradska@naturalint.com>
2017-07-31 18:47:39 +0300

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Calculator {
+public Calculator ();
+public double exec ()
+
+
+
+
+//    public static void main(String[] args) {
+//
+//        Scanner sc = new Scanner(System.in);
+//        try {
+//            System.out.println("Enter first number");
+//            int a = sc.nextInt();
+//            System.out.println("Enter sign");
+//            String sign = sc.next();
+//            System.out.println("Enter second number");
+//            int b = sc.nextInt();
+//            switch (sign) {
+//                case "+":
+//                    System.out.println("result: "+(a + b));
+//                    break;
+//                case "x":
+//                    System.out.println("result: "+(a * b));
+//                    break;
+//                case "/":
+//                    System.out.println("result: "+(a / b));
+//                    break;
+//                case "-":
+//                    System.out.println("result: "+(a - b));
+//                    break;
+//                default:
+//                    System.out.println("Undefined expression");
+//                    break;
+//            }
+//        }catch (Exception e){
+//            System.out.println("Invalid data");
+//        }
+//    }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+import static java.lang.System.*;
+
+public class HelloWorld {
+//    public static void main(String[] args) {
+//        int maxOfArr = 0;
+//        int [] arrayOfInts = {1,123,131,15,651,1,2674, 2674, 54,687,354,684,684,4};
+//        for (int i = 0; i < arrayOfInts.length; i++) {
+//            if (maxOfArr < arrayOfInts[i]) {
+//                maxOfArr = arrayOfInts[i];
+//            }
+//        }
+//        System.out.println(arrayOfInts.length);
+//        System.out.println(maxOfArr);
+//    }
+}
